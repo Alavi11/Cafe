a1dabefc05c03b981a09cec1a7943b8150ff562d
ehsan110alavi <e.alavi1380@gmail.com>
2023-02-24 23:05:24 +0330

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import "./index.css"
 import { Route, Routes } from "react-router-dom"
 import DefaultRoute from "./components/roures/DefaultRoute";
+import Navbar from "./components/navbar/Navbar";
 
 
 function App() {
   return <>
-    <Routes>
-      <Route path="/" element={<DefaultRoute/>} />
-    </Routes>
-</>;
+        <Navbar/>
+        <Routes>
+            <Route path="/" element={<DefaultRoute/>} />
+        </Routes>
+      
+</>
 }
 
 export default App;
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -7,7 +7,7 @@ import Navbar from '../navbar/Navbar';
 const Header = () => {
   return (
     <div className="header">
-        <Navbar/>
+        {/* <Navbar/> */}
         <h1 className="header-title"><DiCoffeescript/>کافه کلاسیک</h1>
         <img src='./img/headerphoto.jpeg' className="header-img"></img>
         <div className="header-opacity"></div>
